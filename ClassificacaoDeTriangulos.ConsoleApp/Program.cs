7993af8a04400fdbc0d97af8be3f78ee88658658
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-18 15:39:41 -0300

    Implementada Versão Inicial

--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Classificação de Triângulos");
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("Informe o valor do lado X: ");
+                decimal ladoX = Decimal.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Informe o valor do lado Y: ");
+                decimal ladoY = Decimal.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Informe o valor do lado Z: ");
+                decimal ladoZ = Decimal.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Valores Informados: ");
+                Console.WriteLine($"Lado X = {ladoX} ");
+                Console.WriteLine($"Lado Y = {ladoY} ");
+                Console.WriteLine($"Lado Z = {ladoZ} ");
+
+
+
+
+
+
+                Console.WriteLine("Deseja COntinuar? (s/n)");
+                string continuar = Console.ReadLine()!.ToUpper();
+
+                if (continuar != "S")
+                    break;
+            }
+            
         }
     }
 }
